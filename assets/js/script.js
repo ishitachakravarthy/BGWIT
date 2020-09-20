618755ae1596586dce1c6923835a73a68c2d6406
ishitachakravarthy <67187955+ishitachakravarthy@users.noreply.github.com>
2020-09-20 17:03:53 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 3,
+  mousewheel: true,
+
+  // If we need pagination
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.previous',
+
+  },
+
+})
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
